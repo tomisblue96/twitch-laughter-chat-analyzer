57df1308ab4f13f6aec0655565942e903d5f828e
tomisblue96 <46725061+tomisblue96@users.noreply.github.com>
2021-12-18 17:33:29 +0900

    코드 가독성 수정

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 # 트위치 시간별 꿀잼 분석기
 
-트위치 다시보기 ID를 입력하면 채팅 로그를 분석하여 시간당 'ㅋ'의 갯수를 xlsx 파일로 출력합니다.
+트위치 다시보기 ID를 입력하면 채팅 로그를 분석하여 시간당 'ㅋ'의 갯수를 엑셀(xlsx) 파일로 출력합니다.
 
 
 
 트위치 다시보기 ID는 다시보기 페이지 URL에서 확인할 수 있습니다.
 
-https://www.twitch.tv/videos/(다시보기 ID)
+www.twitch.tv/videos/(다시보기 ID)
 
 ------
 
@@ -14,4 +14,4 @@ https://www.twitch.tv/videos/(다시보기 ID)
 
 > {"error":"Not Found","status":404,"message":"The v5 API is deprecated and will be shutdown on February 28, 2022. Applications that have not accessed v5 before July 15, 2021 no longer have access to v5. For more information on the v5 API shutdown plan, see https://blog.twitch.tv/2021/07/15/legacy-twitch-api-v5-shutdown-details-and-timeline/ and the Twitch API documentation at https://dev.twitch.tv/docs/api."}
 
-트위치 v5 API가 7월 15일 이후 지원이 되지 않습니다. 트위치의 최신 API는 채팅 내역을 제공하지 않아 코드의 실행이 불가합니다.
+트위치 v5 API가 7월 15일 이후 지원이 되지 않습니다. 트위치의 최신 API는 채팅 내역을 제공하지 않아 코드가 정상적으로 결과를 출력하지 않습니다.
--- chat.py
+++ chat.py
@@ -4,113 +4,83 @@ import json
 import time
 
 
-def doubleDigit(num):
-    if num < 10:
-        return '0' + str(num)
-    else:
-        return str(num)
-
-
-def chatAnalyse(v_id):
-
+def chat_analyse(v_id):
+    # 초기화
     video_id = v_id
     client_id = '*'
 
-    chat = []
-    min = []
-    user = []
-    total = []
-    laugh = []
+    chat_list = []
+    minute_list = []
 
     next_cursor = ''
 
-    params = {}
-    params['client_id'] = client_id
+    params = {'client_id': client_id}
 
     i = 0
     count = 1
 
+    # 다시보기 채팅 내역 가져오기
     while True:
         if i == 0:
-            URL = 'https://api.twitch.tv/v5/videos/' + video_id + '/comments?content_offset_seconds=0'
+            url = 'https://api.twitch.tv/v5/videos/' + video_id + '/comments?content_offset_seconds=0'
             i += 1
         else:
-            URL = 'https://api.twitch.tv/v5/videos/' + video_id + '/comments?cursor=' + next_cursor
+            url = 'https://api.twitch.tv/v5/videos/' + video_id + '/comments?cursor=' + next_cursor
 
         try:
-            response = requests.get(URL, params=params, timeout=2)
+            response = requests.get(url, params=params, timeout=2)
         except:
-            print("에러발생")
-            print("2초후 재시도 시작")
             time.sleep(2)
             continue
 
-        print(count)
         count += 1
 
-        print(response.text)
         j = json.loads(response.text)
 
         for k in range(0, len(j["comments"])):
             timer = j["comments"][k]["content_offset_seconds"]
-            time_minute = int(timer / 60)
-
-            min.append(doubleDigit(time_minute))
-            user.append(j["comments"][k]["commenter"]["display_name"])
-            chat.append(j["comments"][k]["message"]["body"])
+            minute_list.append(timer // 60)
+            chat_list.append(j["comments"][k]["message"]["body"])
 
         if '_next' not in j:
             break
 
         next_cursor = j["_next"]
 
-    print("추출 시작")
-    wb = openpyxl.Workbook()
+    # 가져온 채팅 내역을 엑셀 파일로 내보내기
     wb_total = openpyxl.Workbook()
-    wb_laugh = openpyxl.Workbook()
-    sheet = wb.active
+    wb_laughter = openpyxl.Workbook()
     sheet_total = wb_total.active
-    sheet_laugh = wb_laugh.active
+    sheet_laughter = wb_laughter.active
+    max_minute = minute_list[-1] + 1
 
-    print("로그 추출 시작")
-    sheet.append(["시간", "유저 이름", "채팅", "웃음 개수"])
-    for x in range(0, len(min)):
-        sheet.append([str(min[x]), str(user[x]), str(chat[x]), chat[x].count('ㅋ')])
-
-    wb.save(video_id + '.xlsx')
-    print("로그 추출 완료")
-
-    print("전체 분석 시작")
+    # 시간당 채팅 빈도 파일 만들기
     sheet_total.append(["시간", "채팅 갯수"])
-    for i in range(0, int(min[-1])+1):
-        total.append(0)
-    for x in range(0, len(min)):
-        total[int(min[x])] += 1
-    for x in range(0, len(total)):
-        sheet_total.append([x, total[x]])
+    total_list = [0 for _ in range(max_minute)]
+
+    for minute in minute_list:
+        total_list[minute] += 1
+    for chat_min, chat_sum in enumerate(total_list):
+        sheet_total.append([chat_min, chat_sum])
 
     wb_total.save(video_id + '_total.xlsx')
-    print("전체 분석 완료")
 
-    print("ㅋㅋ 분석 시작")
-    sheet_laugh.append(["시간", "웃음 갯수"])
-    for i in range(0, int(min[-1]) + 1):
-        laugh.append(0)
-    for x in range(0, len(min)):
-        laugh[int(min[x])] += chat[x].count('ㅋ')
-    for x in range(0, len(laugh)):
-        sheet_laugh.append([x, laugh[x]])
+    # 시간당 'ㅋ'의 갯수 빈도 파일 만들기
+    sheet_laughter.append(["시간", "웃음 갯수"])
+    laughter_list = [0 for _ in range(max_minute)]
 
-    wb_laugh.save(video_id + '_laugh.xlsx')
-    print("ㅋㅋ 분석 완료")
+    for minute, chat in zip(minute_list, chat_list):
+        laughter_list[minute] += chat.count('ㅋ')
+    for chat_min, chat_sum in enumerate(laughter_list):
+        sheet_laughter.append([chat_min, chat_sum])
 
-    print("end")
+    wb_laughter.save(video_id + '_laugh.xlsx')
 
 
 def main():
-    while 1:
+    while True:
         vid_num = input("분석할 트위치 다시보기 번호를 입력하세요 : ")
-        chatAnalyse(vid_num)
+        chat_analyse(vid_num)
 
 
 if __name__ == "__main__":
