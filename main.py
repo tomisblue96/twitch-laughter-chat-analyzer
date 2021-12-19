71deca9948e29507a9623ca3e2a1e8a32828c5b8
tomisblue96 <46725061+tomisblue96@users.noreply.github.com>
2021-12-19 18:10:31 +0900

    클라이언트 ID 값 변경 및 엑셀 파일이 아닌 html, csv 파일 출력

--- main.py
+++ main.py
@@ -1,23 +1,42 @@
-import requests
+import csv
 import json
-import time
+import sys
+from time import sleep
 
-import openpyxl
+from bokeh.plotting import figure, output_file, show
+import requests
 
 from client_config import Config
 
 
-def chat_analyse(v_id):
+def min_to_time(minute):
+    hour_str = str(minute // 60)
+    minute_str = str(minute % 60).zfill(2)
+    return f"{hour_str}시 {minute_str}분"
+
+
+def min_to_query(minute):
+    hour_str = str(minute // 60)
+    minute_str = str(minute % 60).zfill(2)
+    return f"?t={hour_str}h{minute_str}m00s"
+
+
+def chat_analyse(id):
     # 초기화
-    video_id = v_id
-    client_id = Config.client_id
+    video_id = id
+    twitch_url = f"https://www.twitch.tv/videos/{id}"
+
+    if not video_id:
+        print("WARNING : 올바른 ID를 입력해주세요!")
+        return
 
     chat_list = []
     minute_list = []
 
     next_cursor = ''
 
-    params = {'client_id': client_id}
+    header = {"Accept": "application/vnd.twitchtv.v5+json; charset=UTF-8"}
+    params = {'client_id': "kimne78kx3ncx6brgo4mv6wki5h1ko"}
 
     i = 0
     count = 1
@@ -31,60 +50,57 @@ def chat_analyse(v_id):
             url = f"https://api.twitch.tv/v5/videos/{video_id}/comments?cursor={next_cursor}"
 
         try:
-            response = requests.get(url, params=params, timeout=60)
+            response = requests.get(url, params=params, headers=header, timeout=10)
         except:
-            time.sleep(2)
+            sleep(1)
             continue
 
         count += 1
 
-        print(response.text)
         j = json.loads(response.text)
+        print(j["comments"][0]['created_at'])
 
-        for k in range(0, len(j["comments"])):
-            timer = j["comments"][k]["content_offset_seconds"]
-            minute_list.append(timer // 60)
-            chat_list.append(j["comments"][k]["message"]["body"])
+        for comment in j["comments"]:
+            timer = float(comment["content_offset_seconds"])
+            minute_list.append(int(timer // 60))
+            chat_list.append(comment["message"]["body"])
 
         if '_next' not in j:
             break
 
         next_cursor = j["_next"]
 
-    # 가져온 채팅 내역을 엑셀 파일로 내보내기
-    wb_total = openpyxl.Workbook()
-    wb_laughter = openpyxl.Workbook()
-    sheet_total = wb_total.active
-    sheet_laughter = wb_laughter.active
-    max_minute = minute_list[-1] + 1
-
-    # 시간당 채팅 빈도 파일 만들기
-    sheet_total.append(["시간", "채팅 갯수"])
-    total_list = [0 for _ in range(max_minute)]
-
-    for minute in minute_list:
-        total_list[minute] += 1
-    for chat_min, chat_sum in enumerate(total_list):
-        sheet_total.append([chat_min, chat_sum])
-
-    wb_total.save(video_id + '_total.xlsx')
-
-    # 시간당 'ㅋ'의 갯수 빈도 파일 만들기
-    sheet_laughter.append(["시간", "웃음 갯수"])
-    laughter_list = [0 for _ in range(max_minute)]
-
+    # 채팅 정보에서 분당 웃음 갯수 세기
+    max_minute = minute_list[-1]
+    laughter_per_min_list = [0 for _ in range(max_minute + 1)]
+    time_list = range(max_minute + 1)
     for minute, chat in zip(minute_list, chat_list):
-        laughter_list[minute] += chat.count('ㅋ')
-    for chat_min, chat_sum in enumerate(laughter_list):
-        sheet_laughter.append([chat_min, chat_sum])
+        laughter_per_min_list[minute] += chat.count('ㅋ')
 
-    wb_laughter.save(video_id + '_laugh.xlsx')
+    # 가져온 채팅 내역을 그래프를 포함한 html 파일로 만들기
+    output_file(f"{video_id}.html")
+    p = figure(title=f"#{video_id} 다시보기 결과",
+               x_axis_label='x',
+               y_axis_label='y',
+               plot_width=1600)
+    p.line(time_list, laughter_per_min_list, legend="분당 웃음 개수", line_width=2)
+    show(p)
+
+    # csv 파일로 저장하기
+    with open(f"{video_id}.csv", "w", newline='') as f:
+        writer = csv.writer(f)
+        csv_list = [[min_to_time(minute), laughter, twitch_url + min_to_query(minute)]
+                    for minute, laughter in zip(time_list, laughter_per_min_list)]
+        csv_list.sort(key=lambda x: -x[1])
+        writer.writerows(csv_list)
+
 
 
 def main():
     while True:
-        vid_num = input("분석할 트위치 다시보기 번호를 입력하세요 : ")
-        chat_analyse(vid_num)
+        print("twitch vod id : ", end='')
+        url = sys.stdin.readline().rstrip()
+        chat_analyse(url)
 
 
 if __name__ == "__main__":
